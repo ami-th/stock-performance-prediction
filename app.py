8466963a8240434e99aff5963f2b099a8f935ee9
Vishnu Latheesh <vishnulatheesh001@gmail.com>
2022-06-29 02:25:39 -0700

    final

--- app.py
+++ app.py
@@ -53,9 +53,28 @@ if selected2 == "Home":
   col1, col2, col3 = st.columns([0.2, 5, 0.2])
   col2.image(image, use_column_width=True)
 
-  st.title('Live Chart')
+  st.title('An introduction to stock markets')
+  st.write('The stock market is a system of trade where shares of companies that are publicly-traded are issued, and can be bought and sold. The stock market can also be viewed as a group of traders who pit their expertise against each other. To begin trading in stocks, you need to create two accounts- a trading account and a DEMAT account. A DEMAT account is where your shares will be stored in a digital format. Your trading account will be used to buy and sell shares.')
 
 
+  image = Image.open('bullsbear.jpg')
+
+  #displaying the image on streamlit app
+  col1, col2, col3 = st.columns([0.1, 2.5, 0.1])
+  col2.image(image, use_column_width=True)
+  st.title('Why do Stock Prices Go Up and Down?')
+  st.write('To know how to understand share market, you need to understand why the stock prices go up and down. Stock prices are influenced by multiple factors that determine whether they will go up or down. Factors like media, opinions of famous investors, political upheaval, natural calamities, risk factors, and supply and demand. The complex interaction of these factors, along with all relevant information about the stocks, is responsible for the creation of a specific kind of sentiment, and a resultant number of sellers and buyers. If the number of sellers is more than the buyers, the prices tend to fall. When the opposite happens, prices generally rise.')
+  st.write("##")
+
+
+  #Chart analysis
+
+  image = Image.open('chart_analysis.png')
+  #displaying the image on streamlit app
+  col1, col2, col3 = st.columns([0.1, 2.5, 0.1])
+  col2.image(image, use_column_width=True)
+  st.title('Why is it so difficult to predict the Stock Market?')
+  st.write('Let’s imagine a scenario where there has been an increase in the stock prices for years. Investors know that a correction is round the corner which will upset the stock prices. What is unknown is the ‘what’ and the ‘when’- what will trigger it, and when will it happen. In that scenario, what can we do? Some will sit back with cash in hand, waiting for a suitable time to enter the trade. Some will be willing to take the risk and jump in. Now, the questions are- if you are waiting, how will you identify the right time to begin the trade? And how will you know when to exit it? In a world where the stock market was predictable, understanding the stock market would have been easier.')
 
 #Auto-Trade
 
@@ -63,7 +82,7 @@ if selected2 == "Auto-Trade":
 
   #opening the image
 
-  image = Image.open('Auto trade.png')
+  image = Image.open('auto_trade.png')
 
 
 
@@ -73,7 +92,7 @@ if selected2 == "Auto-Trade":
 
 
 
-  obj = SmartConnect(api_key = 'B94qZLC7')
+  obj = SmartConnect(api_key = 'iSZBbdiG')
   data = obj.generateSession('P714176', 'P@ssw0rd@123')
 
   def place_order(orderparams):
@@ -126,6 +145,16 @@ if selected2 == "Auto-Trade":
               for trade in trade_list:
                   place_order(trade)
 
+  #opening the image
+
+  image = Image.open('table.png')
+
+
+
+  #displaying the image on streamlit app
+  col1, col2, col3 = st.columns([0.2, 5, 0.2])
+  col2.image(image, use_column_width=True)                 
+
 
 
 #Prediction
@@ -135,8 +164,28 @@ if selected2 == "Prediction":
   START = "2015-01-01"
   TODAY = date.today().strftime("%Y-%m-%d")
   st.title('Stock Forecast App')
-  stocks = ('GOOGL', 'AAPL', 'MSFT', 'TSLA', 'AMZN', 'FB', 'NFLX', 'BTC-USD')
-  selected_stock = st.selectbox('Select dataset for prediction', stocks)
+  stocks = ('AAPL', 'ABBOTINDIA.NS', 'ADANIENT.NS', 'ADANIPORTS.NS',
+            'ADANIPOWER.NS', 'ADBE', 'ADSK', 'AEP', 'ALGN',
+            'AMA', 'AMD', 'AMGN', 'AMZN', 'ANSS', 'APOLLOHOSP.NS',
+            'ARGU', 'ASIANPAINT.NS', 'ASML', 'AVGO', 'AXISBANK.NS',
+            'BAJAJ-AUTO.NS', 'BAJFINANCE.NS', 'BHARTIARTL.NS',
+            'BIIB', 'BOSCHLTD.NS', 'BPCL.NS', 'BRITANNIA.NS', 'CANBK.NS',
+            'CDNS', 'CIPLA.NS', 'CMCSA', 'COALINDIA.NS', 'COST', 'CPRT',
+            'CRWD', 'CSCO', 'CSX', 'CTAS', 'CTSH', 'DIS', 'DMLP', 'DO',
+            'DOCU', 'DRREDDY.NS', 'DXCM', 'EA', 'EICHERMOT.NS', 'EXC',
+            'FAST', 'FB', 'GAIL.NS', 'GILD', 'GOOGL', 'GRASIM.NS', 
+            'HCLTECH.NS', 'HDFCBANK.NS', 'HDIL.NS', 'HEROMOTOCO.NS',
+            'HINDUNILVR.NS', 'HON', 'ICICIBANK.NS', 'IDXX', 'ILMN',
+            'INFY.NS', 'INTC', 'INTU', 'IOC.NS', 'ITC.NS', 'JD', 'JINDALSTEL.NS',
+            'JSWSTEEL.NS', 'KO', 'KOTAKBANK.NS', 'LT.NS', 'LTI.NS', 'M&M.NS',
+            'MARUTI.NS', 'MCHP', 'MELI', 'MRVL', 'MSFT', 'MUTHOOTFIN.NS',
+            'NESTLEIND.NS', 'NFLX', 'NTPC.NS', 'NVDA', 'OLPX', 'ONGC.NS',
+            'PEP', 'PNB.NS', 'POWERGRID.NS', 'PYPL', 'RELIANCE.NS', 'SBIN.NS',
+            'SHREECEM.NS', 'SIDU', 'SNPS', 'TATACONSUM.NS', 'TATAMOTORS.NS', 
+            'TATASTEEL.NS', 'TCS.NS', 'TEAM', 'TECHM.NS', 'TSLA', 'TXN', 
+            'ULTRACEMCO.NS', 'UPL.NS', 'VTNR','WIPRO.NS')
+  
+  selected_stock = st.selectbox('Select the Stock of a Company', stocks)
   n_years = st.slider('Years of prediction:', 1, 4)
   period = n_years * 365
 
@@ -153,16 +202,16 @@ if selected2 == "Prediction":
   data = load_data(selected_stock)
   data_load_state.text('Loading data... done!')
 
-  st.subheader('Raw data')
+  st.subheader('Collected Data of the selected company')
   st.write(data.tail())
 
 #Plot raw data
 
   def plot_raw_data():
 	  fig = go.Figure()
-	  fig.add_trace(go.Scatter(x=data['Date'], y=data['Open'], name="stock_open"))
-	  fig.add_trace(go.Scatter(x=data['Date'], y=data['Close'], name="stock_close"))
-	  fig.layout.update(title_text='Time Series data with Rangeslider', xaxis_rangeslider_visible=True)
+	  fig.add_trace(go.Scatter(x=data['Date'], y=data['Open'], name="open"))
+	  fig.add_trace(go.Scatter(x=data['Date'], y=data['Close'], name="close"))
+	  fig.layout.update(title_text='Opening & Closing Price Of Collected Data', xaxis_rangeslider_visible=True)
 	  st.plotly_chart(fig)
 	
   plot_raw_data()
@@ -177,14 +226,14 @@ if selected2 == "Prediction":
   forecast = m.predict(future)
 
 # Show and plot forecast
-  st.subheader('Forecast data')
+  st.subheader('Forecasted data')
   st.write(forecast.tail())
     
-  st.write(f'Forecast plot for {n_years} years')
+  st.write(f'Predictions of {n_years} year')
   fig1 = plot_plotly(m, forecast)
   st.plotly_chart(fig1)
 
-  st.write("Forecast components")
+  st.write("Trend, Weekly, Yearly Charts")
   fig2 = m.plot_components(forecast)
   st.write(fig2)
 
@@ -193,9 +242,28 @@ if selected2 == "Prediction":
  #Compare
 
 if selected2 == "Compare":
-  st.title('Compare-O-Meter')
+  st.title('Compare Assets')
 
-  tickers = ('TSLA', 'AAPL', 'MSFT', 'BTC-USD', 'ETH-USD','NFLX')
+  tickers = ('AAPL', 'ABBOTINDIA.NS', 'ADANIENT.NS', 'ADANIPORTS.NS',
+            'ADANIPOWER.NS', 'ADBE', 'ADSK', 'AEP', 'ALGN',
+            'AMA', 'AMD', 'AMGN', 'AMZN', 'ANSS', 'APOLLOHOSP.NS',
+            'ARGU', 'ASIANPAINT.NS', 'ASML', 'AVGO', 'AXISBANK.NS',
+            'BAJAJ-AUTO.NS', 'BAJFINANCE.NS', 'BHARTIARTL.NS',
+            'BIIB', 'BOSCHLTD.NS', 'BPCL.NS', 'BRITANNIA.NS', 'CANBK.NS',
+            'CDNS', 'CIPLA.NS', 'CMCSA', 'COALINDIA.NS', 'COST', 'CPRT',
+            'CRWD', 'CSCO', 'CSX', 'CTAS', 'CTSH', 'DIS', 'DMLP', 'DO',
+            'DOCU', 'DRREDDY.NS', 'DXCM', 'EA', 'EICHERMOT.NS', 'EXC',
+            'FAST', 'FB', 'GAIL.NS', 'GILD', 'GOOGL', 'GRASIM.NS', 
+            'HCLTECH.NS', 'HDFCBANK.NS', 'HDIL.NS', 'HEROMOTOCO.NS',
+            'HINDUNILVR.NS', 'HON', 'ICICIBANK.NS', 'IDXX', 'ILMN',
+            'INFY.NS', 'INTC', 'INTU', 'IOC.NS', 'ITC.NS', 'JD', 'JINDALSTEL.NS',
+            'JSWSTEEL.NS', 'KO', 'KOTAKBANK.NS', 'LT.NS', 'LTI.NS', 'M&M.NS',
+            'MARUTI.NS', 'MCHP', 'MELI', 'MRVL', 'MSFT', 'MUTHOOTFIN.NS',
+            'NESTLEIND.NS', 'NFLX', 'NTPC.NS', 'NVDA', 'OLPX', 'ONGC.NS',
+            'PEP', 'PNB.NS', 'POWERGRID.NS', 'PYPL', 'RELIANCE.NS', 'SBIN.NS',
+            'SHREECEM.NS', 'SIDU', 'SNPS', 'TATACONSUM.NS', 'TATAMOTORS.NS', 
+            'TATASTEEL.NS', 'TCS.NS', 'TEAM', 'TECHM.NS', 'TSLA', 'TXN', 
+            'ULTRACEMCO.NS', 'UPL.NS', 'VTNR','WIPRO.NS')
 
   dropdown = st.multiselect('Pick your assets', tickers)
 
@@ -217,7 +285,7 @@ if selected2 == "Alert":
 
     #opening the image
 
-  image = Image.open('Alert.png')
+  image = Image.open('alert.png')
 
 
 
@@ -225,9 +293,7 @@ if selected2 == "Alert":
   col1, col2, col3 = st.columns([0.2, 5, 0.2])
   col2.image(image, use_column_width=True)
 
-
-
-  import streamlit as st 
-  st.title("Get Evey Price Actions!")
-  st.write("[Join our Telegram Channel to get live upadtes Of Market Movements!](https://t.me/sppalert)")
-  
+ 
+  st.title("STOCK ALERTS SO YOU CATCH THE ACTION LIVE!")
+  st.write('Never again settle for setting limit orders before going to bed and hoping for the best. Stop checking your phone during work to find yet another lost opportunity. We have made a Bot for keeping you Updated, Join our '"  ""[Telegram Channel](https://t.me/sppalert)"" " 'to get real-time price alerts! Or Follow us on'" ""[Twitter](https://twitter.com/sppalert)"" "'to get real-time updates')
+  st.write("")
